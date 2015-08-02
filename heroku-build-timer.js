13c1fb3345376cd22d0500a0aefc399b41bf933a
Simon Fry <simon@frys.me.uk>
2015-08-02 15:57:40 +0100

    Super inefficient way of getting slug size.

--- heroku-build-timer.js
+++ heroku-build-timer.js
@@ -1,14 +1,24 @@
 if (Meteor.isClient) {
-  Session.setDefault('data', []);
+  Session.setDefault('builds', []);
+  Session.setDefault('slugs', {});
   Session.setDefault('currentApp', "");
 
   var chart;
 
   function fetchAndSetApp (appName) {
-    Meteor.call('getHerokuData', appName, function (error, result) {
+    Meteor.call('getHerokuBuildData', appName, function (error, result) {
       if (!error) {
         Session.set('currentApp', appName);
-        Session.set('data', result);
+        Session.set('builds', result);
+        result.forEach(function (build) {
+          if (build.slug.id) {
+            Meteor.call('getHerokuSlugData', appName, build.slug.id, function (error, result) {
+              var tempSlugs = Session.get('slugs');
+              tempSlugs[result.id] = result.size;
+              Session.set('slugs', tempSlugs);
+            });
+          }
+        });
       }
     });
     Meteor.call('getGraphData', appName, function (error, result) {
@@ -25,8 +35,8 @@ if (Meteor.isClient) {
   }
 
   Template.main.helpers({
-    data: function () {
-      return Session.get('data');
+    builds: function () {
+      return Session.get('builds');
     },
     primaryApp: function () {
       return Meteor.settings.public.primaryApp
@@ -62,14 +72,22 @@ if (Meteor.isClient) {
       var end = moment(this.updated_at);
       var duration = moment.duration(end.diff(start));
       return duration.minutes() + "m " + duration.seconds() + "s";
+    },
+    slugSize: function () {
+      return parseInt(Session.get('slugs')[this.slug.id] / (1024 * 1024));
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.methods({
-    getHerokuData: function (appName) {
+    getHerokuBuildData: function (appName) {
       var response = herokuBuildsAPI(appName, 10);
+
+      return response.data;
+    },
+    getHerokuSlugData: function (appName, slugId) {
+      var response = herokuSlugAPI(appName, slugId);
 
       return response.data;
     },
@@ -101,6 +119,19 @@ if (Meteor.isServer) {
         headers: {
           accept: 'application/vnd.heroku+json; version=3',
           range: 'started_at ..; order=desc,max=' + numberOfResults + ';',
+          authorization: 'Bearer ' + Meteor.settings.herokuToken
+        }
+      }
+    );
+
+    return response;
+  }
+
+  function herokuSlugAPI (appName, slugId) {
+    var response = HTTP.get('https://api.heroku.com/apps/' + appName + '/slugs/' + slugId,
+      {
+        headers: {
+          accept: 'application/vnd.heroku+json; version=3',
           authorization: 'Bearer ' + Meteor.settings.herokuToken
         }
       }
